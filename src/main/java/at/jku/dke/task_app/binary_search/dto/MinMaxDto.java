015f7791a1ff970920ca303e12125c742d590b9d
Martin <martin@straszer.eu>
2024-01-13 18:52:48 +0100

    ci: Fixed GitHub Actions

--- src/main/java/at/jku/dke/task_app/binary_search/dto/MinMaxDto.java
+++ src/main/java/at/jku/dke/task_app/binary_search/dto/MinMaxDto.java
@@ -1,10 +1,12 @@
 package at.jku.dke.task_app.binary_search.dto;
 
+import jakarta.validation.constraints.NotNull;
+
 /**
  * Contains a minimum and a maximum number.
  *
  * @param min The minimum number.
  * @param max The maximum number.
  */
-public record MinMaxDto(int min, int max) {
+public record MinMaxDto(@NotNull int min, @NotNull int max) {
 }
